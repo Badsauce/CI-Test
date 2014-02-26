d008f47b63be089a231473b0fae3cb1883f27481
badsauce <ninjazoby@gmail.com>
2014-02-26 16:56:39 -0500

    basic jenkins build configured

---
+++ src/hello.php
@@ -0,0 +1,6 @@
+<?php
+function say_hi(){
+    return "Hello world!";
+}
+
+?>
---
+++ tests/unit/helloTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class helloTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testHello(){
+        $testString = say_hi();
+
+        $this->assertEquals($testString, "Hello world!");
+    }
+
+}
+
+?>
