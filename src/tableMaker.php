0188c60ebb5cddd18aba530b909e84ec2a57d65b
badsauce <ninjazoby@gmail.com>
2014-03-05 12:06:02 -0500

    added more functions to table maker

--- src/tableMaker.php
+++ src/tableMaker.php
@@ -8,6 +8,30 @@ class tableMaker{
     public function endTable(){
         return "</table>";
     }
+
+    public function makeHeaders($headers){
+        $output = '<tr>';
+            foreach ($headers as $header){
+                $output .= '<th>'.$header.'</th>';
+            }
+        return $output.'</tr>';
+    }
+
+    public function makeRow($row){
+        $output = '<tr>';
+            foreach($row as $cell){
+                $output .= '<td>'.$cell.'</td>';
+            }
+        return $output.'</tr>';
+    }
+
+    public function makeRows($rows){
+        $output = '';
+        foreach($rows as $row){
+            $output .= makeRow($row);
+        }
+        return $output;
+    }
 }
 
 ?>
